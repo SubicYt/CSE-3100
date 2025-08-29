14f8c309240bdee34186a65a1f8b93fd850ba3bd
Ethan Denning <96307838+SubicYt@users.noreply.github.com>
2025-08-29 12:25:51 +0000

    updated main.c for summing

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello world");
+
+    printf("Hello world\n");
+    int indexer = 0;
+    int sum;
+    while(indexer < 200){
+        sum += indexer;
+        indexer++;
+    }
+
+    printf("%d", sum);
     return 0;
 }
