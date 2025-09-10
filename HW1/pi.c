e15ea375514e53764e3b7174666d1b71bd03855c
Ethan Denning <96307838+SubicYt@users.noreply.github.com>
2025-09-09 22:59:00 -0400

    Update pi.c

--- HW1/pi.c
+++ HW1/pi.c
@@ -22,11 +22,11 @@ int main()
 	//add code below 
 
     for(i = 0; i<n; i++){
-        double var1 = 4.0 / (8.0*i + 1.0);
-        double var2 = 2.0 / (8.0*i + 4.0);
-        double var3 = 1.0 / (8.0*i + 5.0);
-        double var4 = 1.0 / (8.0*i + 6.0);
-        pi += (var1 - var2 - var3 - var4) / power(16, i);
+        double term = (4.0 / (8.0*i + 1.0)
+                     - 2.0 / (8.0*i + 4.0)
+                     - 1.0 / (8.0*i + 5.0)
+                     - 1.0 / (8.0*i + 6.0));
+        pi += term / power(16, i);
     }
 
 	printf("PI = %.10f\n", pi);
