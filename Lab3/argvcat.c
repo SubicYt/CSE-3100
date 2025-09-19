ae1e1b3ba9220a8ce154a4ceabd1760db286745b
Ethan Denning <96307838+SubicYt@users.noreply.github.com>
2025-09-19 09:48:34 -0400

    Update argvcat.c

--- Lab3/argvcat.c
+++ Lab3/argvcat.c
@@ -15,19 +15,36 @@ void my_error(char *s)
  */
 char *my_strcat(char *s1, char *s2)
 {
-    // TODO 
-    return NULL;
+    size_t len = strlen(s1) + strlen(s2) + 1;
+    char *mystr = malloc(len);
+    if (mystr == NULL){
+        my_error("malloc failed");
+    }
+
+    strcpy(mystr, s1); // starting string
+    strcat(mystr, s2); // concatenates to the rest of mystr
+    
+    return mystr;
 }
 
 int main(int argc, char *argv[])
 {
-    char    *s;
+    char *s;
+    char *temp;
 
     s = my_strcat("", argv[0]);
 
     for (int i = 1; i < argc; i ++) {
+        temp = s;
         s = my_strcat(s, argv[i]);
+        free(temp);
     }
+
+    printf("%s\n", s);
+
+    free(s);
+    return 0;
+}
 
     printf("%s\n", s);
 
